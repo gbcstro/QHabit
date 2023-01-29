dd784a71360fdd678279a0e78aec6b89efe54385
Gabriel Castro <123627551+gb0jt@users.noreply.github.com>
2023-01-29 21:05:48 +0800

    new authservice

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -7,57 +7,11 @@ import {
   AngularFirestoreDocument,
 } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
-import { Injectable, NgZone } from '@angular/core';
-import { User } from '../services/user';
-import * as auth from 'firebase/auth';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import {
-  AngularFirestore,
-  AngularFirestoreDocument,
-} from '@angular/fire/compat/firestore';
-import { Router } from '@angular/router';
 @Injectable({
-  providedIn: 'root',
   providedIn: 'root',
 })
 export class AuthenticationService {
-
-  userData: any; 
-
-  constructor(
-    public afs: AngularFirestore, 
-    public afAuth: AngularFireAuth, 
-    public router: Router,
-    public ngZone: NgZone 
-  ) {
-    
-    this.afAuth.authState.subscribe((user) => {
-      if (user) {
-        this.userData = user;
-        localStorage.setItem('user', JSON.stringify(this.userData));
-        JSON.parse(localStorage.getItem('user')!);
-      } else {
-        localStorage.setItem('user', 'null');
-        JSON.parse(localStorage.getItem('user')!);
-      }
-    });
-  }
-  
-  SignIn(email: string, password: string) {
-    return this.afAuth
-      .signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        this.SetUserData(result.user);
-        this.afAuth.authState.subscribe((user) => {
-          if (user) {
-            this.router.navigate(['habit-list']);
-          }
-        });
-      })
-      .catch((error) => {
-        window.alert(error.message);
-      });
-  userData: any; 
+  userData: any;
 
   constructor(
     public afs: AngularFirestore, 
@@ -65,7 +19,6 @@ export class AuthenticationService {
     public router: Router,
     public ngZone: NgZone 
   ) {
-    
     this.afAuth.authState.subscribe((user) => {
       if (user) {
         this.userData = user;
@@ -101,7 +54,7 @@ export class AuthenticationService {
         this.SetUserData(result.user);
         this.afAuth.authState.subscribe((user) => {
           if (user) {
-            this.router.navigate(['login']);
+            this.router.navigate(['']);
           }
         });
       })
@@ -114,14 +67,27 @@ export class AuthenticationService {
     const user = JSON.parse(localStorage.getItem('user')!);
     return user !== null && user.emailVerified !== false ? true : false;
   }
-  
-  SetUserData(user: any) {
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
-      `users/${user.uid}`
+
   SetUserData(user: any) {
     const userRef: AngularFirestoreDocument<any> = this.afs.doc(
       `users/${user.uid}`
     );
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      emailVerified: user.emailVerified,
+    };
+    return userRef.set(userData, {
+      merge: true,
+    });
   }
 
+  SignOut() {
+    return this.afAuth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['']);
+    });
+  }
 }
